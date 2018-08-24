12110b0fc1d12b9d29f870194702a6fc55a7eeed
Dennis Smink <dennissmink@gmail.com>
2018-08-24 10:25:00 +0200

    Added NL

---
+++ nl/validation.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default error messages used by
+    | the validator class. Some of these rules have multiple versions such
+    | as the size rules. Feel free to tweak each of these messages here.
+    |
+    */
+
+    'attached' => 'Het :attribute veld is al gekoppeld.',
+    'relatable' => 'Het :attribute veld mag niet gekoppeld worden met dit model.',
+
+];
