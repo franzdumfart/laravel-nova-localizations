a958f884275ae1bf7aedd3ce218842891ebdb2b3
Martin Acosta <tinthenet@users.noreply.github.com>
2018-08-24 10:14:47 -0300

    Create validation.php
    Spanish Translate

---
+++ lang/es/validation.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default error messages used by
+    | the validator class. Some of these rules have multiple versions such
+    | as the size rules. Feel free to tweak each of these messages here.
+    |
+    */
+    'attached' => 'Este :attribute ya está adjunto.',
+    'relatable' => 'Este :attribute puede no estar asociado con este recurso.',
+];
