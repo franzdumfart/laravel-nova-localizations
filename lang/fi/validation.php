da3d3745cdf5b849f2da1a64fed0a37d070dd557
Gil Gomes <gil@gilgomes.com>
2018-10-09 18:02:47 +0100

    cloned master to reflect lastest changes

--- lang/fi/validation.php
+++ lang/fi/validation.php
@@ -11,9 +11,9 @@ return [
     | the validator class. Some of these rules have multiple versions such
     | as the size rules. Feel free to tweak each of these messages here.
     |
- */
+    */
 
-    'attached' => 'Αυτό το :attribute είναι ήδη συνημμένο.',
-    'relatable' => 'Αυτή η :attribute ενδέχεται να μην συσχετίζεται με αυτόν τον πόρο.',
+    'attached' => 'Tämä :attribute on jo liitetty.',
+    'relatable' => 'Tämä :attribute ei voi liittää tähän resurssiin.',
 
 ];
