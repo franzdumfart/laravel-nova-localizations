a49cffbde29081ea58f6ec07c2e4d1eabda86e13
Georgi Kolev <suncho_26@aol.com>
2019-07-29 10:17:10 +0200

    Adding bulgarian translation

---
+++ lang/bg/validation.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default error messages used by
+    | the validator class. Some of these rules have multiple versions such
+    | as the size rules. Feel free to tweak each of these messages here.
+    |
+    */
+
+    'attached' => 'Поле :attribute вече се използва.',
+    'relatable' => 'Поле :attribute не може да бъде свързано с този ресурс.',
+
+];
